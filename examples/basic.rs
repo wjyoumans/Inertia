82b71942d7b7e874a0c5be45e9b98c538626e13d
wyoumans <wyoumans@mail.usf.edu>
2022-01-16 21:09:34 -0500

    update cargo.toml and example

--- examples/basic.rs
+++ examples/basic.rs
@@ -3,6 +3,6 @@ use inertia::prelude::*;
 
 fn main() {
     let rr = RationalField::init();
-    let rx = PolyRing::new(rr, "x");
+    let rx = PolyRing::<RationalField>::init("x");
     let r = rx.new(1);
 }
